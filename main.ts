05a7ab7a5dfd9ec9d4fe608e703c6e71aa26dbc6
sontakkea610-hub <sontakkea610@gmail.com>
2026-07-11 21:31:54 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 basic.showString("ARYAN SONTAKKE")
 basic.showIcon(IconNames.Heart)
+basic.clearScreen()
 basic.forever(function () {
     basic.showNumber(1)
+    basic.pause(100)
+    basic.showArrow(ArrowNames.NorthEast)
 })
